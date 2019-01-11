5fc93d613fe3c4ae4a3af3a5925a1ed1d39547a3
Julius <jubeki99@gmail.com>
2019-01-11 12:17:01 +0100

    Add Comments to routes

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,28 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Add routes for authentication
 Auth::routes();
 
+// Welcome Page with a List of registerable Camps
 Route::get('/', 'WelcomeController@index');
 
+// Legal Links
 Route::view('/teilnahmebedingungen', 'legal.terms');
 Route::view('/datenschutz', 'legal.privacy');
 Route::view('/impressum', 'legal.imprint');
 
+// Profile for authenticated users
 Route::get('/profile', 'ProfileController@index')->name('profile');
 Route::post('/profile', 'ProfileController@update');
 
+// Camp Links
 Route::get('mycamps/create/{camp}', 'CampUserController@create');
 Route::resource('mycamps', 'CampUserController');
 Route::resource('camps', 'CampController');
 
-
+// Admin Links
 Route::group(['middleware' => 'can:isAdmin'], function() {
     Route::get('/adminpanel/dashboard', 'CampAdminController@index');
     Route::resource('admin', 'CampAdminController');
