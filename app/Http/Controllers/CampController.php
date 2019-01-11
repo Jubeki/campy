002c557c77957322f754c8256db99bd89f24541a
Julius <jubeki99@gmail.com>
2019-01-11 12:23:19 +0100

    Update comments in CampController

--- app/Http/Controllers/CampController.php
+++ app/Http/Controllers/CampController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 class CampController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a list of registerable Camps
      *
      * @return \Illuminate\Http\Response
      */
