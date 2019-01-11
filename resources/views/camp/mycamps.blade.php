b4fcaa6e6dc71c52ceae4410567c6fd8473329c3
Julius <jubeki99@gmail.com>
2019-01-11 15:07:06 +0100

    Format camp.mycamps

--- resources/views/camp/mycamps.blade.php
+++ resources/views/camp/mycamps.blade.php
@@ -2,35 +2,41 @@
 
 @section('content')
 <div class="mb-4">
-    <div class="mb-4"><h2>Meine Camps</h2>
+    <div class="mb-4">
+        <h2>Meine Camps</h2>
         <p class="text-sm">Nur die Camps anzeigen, für die ich registriert bin.</p>
-        <p class="text-sm mt-2">Über den Link <strong>Details</strong> kannst du bei jedem Camp den Fortschritt deiner Anmeldung sehen (Laptop, Bezahlung), innerhalb der Frist deine Daten ändern und absagen.</p>
+        <p class="text-sm mt-2">
+            Über den Link <strong>Details</strong> kannst du bei jedem Camp den Fortschritt deiner Anmeldung sehen
+            (Laptop, Bezahlung), innerhalb der Frist deine Daten ändern und absagen.
+        </p>
     </div>
 
-<div class="table-responsive"><table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Stadt</th>
-      <th scope="col">Von</th>
-      <th scope="col">Bis</th>
-      <th scope="col">Status</th>
-      <th scope="col">Laptop?</th>
-      <th scope="col">Optionen</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($camp_user as $camp)
-    <tr>
-      <td class="font-bold"><a href="mycamps/{{ $camp->id }}/edit">{{ $camp->city }}</a></th>
-      <td>{{ $camp->from->format('d.m.Y') }}</td>
-      <td>{{ $camp->to->format('d.m.Y') }}</td>
-      <td>@component('components.status_camp', ['camp' => $camp->pivot->status]) @endcomponent</td>
-      <td>@component('components.status_laptop', ['laptop' => $camp->pivot->laptop]) @endcomponent</td>
-      <td><a href="{{route('camp', [$camp])}}">Details</a></td>
-    </tr>
-    @endforeach
-  </tbody>
-</table></div>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Stadt</th>
+                    <th scope="col">Von</th>
+                    <th scope="col">Bis</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Laptop?</th>
+                    <th scope="col">Optionen</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($camp_user as $camp)
+                    <tr>
+                        <td class="font-bold"><a href="mycamps/{{ $camp->id }}/edit">{{ $camp->city }}</a></th>
+                        <td>{{ $camp->from->format('d.m.Y') }}</td>
+                        <td>{{ $camp->to->format('d.m.Y') }}</td>
+                        <td>@component('components.status_camp', ['camp' => $camp->pivot->status]) @endcomponent</td>
+                        <td>@component('components.status_laptop', ['laptop' => $camp->pivot->laptop]) @endcomponent</td>
+                        <td><a href="{{route('camp', [$camp])}}">Details</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
 </div>
 @endsection
