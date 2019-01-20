64d56b98f888c72f825218451350a6704c703ab0
Julius <jubeki99@gmail.com>
2019-01-20 16:51:43 +0100

    Fix post-route to profile

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Auth::routes();
 
 // Route::get('/', function () { return view('welcome'); });
 Route::get('/profile', 'ProfileController@index')->name('profile');
-Route::post('/profile', 'ProfileController@update');
+Route::patch('/profile', 'ProfileController@update');
 Route::get('/teilnahmebedingungen', function() { return view('legal.terms');} );
 Route::get('/datenschutz', function() { return view('legal.privacy');} );
 Route::get('/impressum', function() { return view('impressum');} );
