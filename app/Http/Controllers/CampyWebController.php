3a1cedc37fc35c79055657f2356cf76838a7f80d
Julius <jubeki99@gmail.com>
2019-01-22 17:47:56 +0100

    Add routes to CampyWebController

--- app/Http/Controllers/CampyWebController.php
+++ app/Http/Controllers/CampyWebController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class CampyWebController extends Controller
 {
-    //
+    public function camp_registration(Request $request) {
+
+    }
+
+    public function gewinnspiel(Request $request) {
+
+    }
+
+    public function magazin(Request $request) {
+
+    }
+
+    public function kontakt(Request $request) {
+
+    }
+
+    public function interest(Request $request) {
+
+    }
 }
